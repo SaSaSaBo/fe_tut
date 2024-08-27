27043365b2734c5dacf6bffa45ebd482104a510b
SaSaSaBo <sasasabo14@gmail.com>
2024-08-27 10:30:40 +0300

    React 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,132 @@
-import './some.css';
-// We use these css code for Tailwind.tsx code. And after the library that we add <sass> to our project we  are gonna use he style.scss.
-import { createElement, useState } from 'react';
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import './some.css';
+// // We use these css code for Tailwind.tsx code. And after the library that we add <sass> to our project we  are gonna use he style.scss.
+// import { createElement, useState } from 'react';
 
-// import './style.scss'; // instead of using this we're gonna use some.css Cause we're gonna made Button component.
+// // import './style.scss'; // instead of using this we're gonna use some.css Cause we're gonna made Button component.
 
-import Button from './components/Button';
-import Tab from './components/Tab';
+// import Button from './components/Button';
+// import Tab from './components/Tab';
 
-/*
-interface ButtonProps {
-  text: string; // 'text' adında bir string prop bekliyoruz
-}
+// /*
+// interface ButtonProps {
+//   text: string; // 'text' adında bir string prop bekliyoruz
+// }
 
-function Button(props: ButtonProps) {
-  return <button>{props.text}</button>
-}
-*/
+// function Button(props: ButtonProps) {
+//   return <button>{props.text}</button>
+// }
+// */
 
+
+// function App() {
+
+//   const [activeTab, setActiveTab] = useState(1);
+
+//   const todos = ['Gunil', 'Jungsu', 'Gaon', 'O.de', 'Jun Han', 'Jooyeon'];
+  
+
+//   const searchFunction = () => {
+//     alert('Searching...');
+//   }
+// /*  
+//   // tsx style written list.
+
+//   const h1 = createElement('h1', null, 'sasaStaaaLlain.com');
+
+//   const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)));
+
+//   const button = createElement(Button, {
+//     text: 'XdinaryHeroes'}, null);
+
+//   return createElement('main', {
+//     className: 'test',
+//     id: 'main'
+//   }, h1, ul, button)
+
+
+//   //html style written list
+//     return (
+//     // After creating the Style.tsx we move div from there
+//     <>
+//       <Button text="XDINARYHEROES" />
+//       <h1>sasaStaaaLlain.com</h1>
+//       <ul>
+//         {
+//           todos.map(todo => 
+//             <li>
+//             {todo}
+//           </li>
+//         )}
+//       </ul>
+//     </>
+//   );
+// */
+
+//   return (
+//     // After creating the Style.tsx we move div from there
+//     <>
+
+//       <div style={{padding: '20px'}}>
+
+//         <button onClick={() => setActiveTab(2)}>Change activeTab</button>
+
+//         <Tab activeTab={activeTab} setActiveTab={setActiveTab}> 
+//           <Tab.Panel title="skz">StrayKids</Tab.Panel>
+//           <Tab.Panel title="xh">XdinaryHeroes</Tab.Panel>
+//           <Tab.Panel title="xg">XdinaryGirls</Tab.Panel>
+//         </Tab>
+//       </div>
+
+//       <div style={{padding: '20px'}}>
+
+//         {/* <Button text="XDINARYHEROES" /> */}
+//         <Button> XDINARYHEROES&STRAYKIDS1 </Button> 
+//         {/* when its like that the text called 'children' */}
+//         <br />
+//         <Button variant="success">XDINARYHEROES&STRAYKIDS2</Button>
+//         <br />
+//         <Button  variant="warning">XDINARYHEROES&STRAYKIDS3</Button>
+//         <br />
+//         <Button  variant="danger">XDINARYHEROES&STRAYKIDS4</Button>
+      
+//       </div>
+
+//       <h1 style={{color: 'white', background: 'red'}}>sasaStaaaLlain.com</h1>
+
+//       <br />
+
+//       <label htmlFor="search" onClick={searchFunction}>Search</label>
+//       <input type="text" id="search" style={{background: 'grey'}} />
+
+//       <ul>
+//         {
+//           todos.map(todo => 
+//             <li>
+//             {todo}
+//           </li>
+//         )}
+//       </ul>
+
+//     </>
+//   )
+// }
+
+// export default App;
+
+import Test from './Test';
+import React, { useState } from 'react'
 
 function App() {
 
-  const [activeTab, setActiveTab] = useState(1);
-
-  const todos = ['Gunil', 'Jungsu', 'Gaon', 'O.de', 'Jun Han', 'Jooyeon'];
-  
-
-  const searchFunction = () => {
-    alert('Searching...');
-  }
-/*  
-  // tsx style written list.
-
-  const h1 = createElement('h1', null, 'sasaStaaaLlain.com');
-
-  const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)));
-
-  const button = createElement(Button, {
-    text: 'XdinaryHeroes'}, null);
-
-  return createElement('main', {
-    className: 'test',
-    id: 'main'
-  }, h1, ul, button)
-
-
-  //html style written list
-    return (
-    // After creating the Style.tsx we move div from there
-    <>
-      <Button text="XDINARYHEROES" />
-      <h1>sasaStaaaLlain.com</h1>
-      <ul>
-        {
-          todos.map(todo => 
-            <li>
-            {todo}
-          </li>
-        )}
-      </ul>
-    </>
-  );
-*/
+  const [show, setShow] = useState(false);
 
   return (
-    // After creating the Style.tsx we move div from there
     <>
-
-      <div style={{padding: '20px'}}>
-
-        <button onClick={() => setActiveTab(2)}>Change activeTab</button>
-
-        <Tab activeTab={activeTab} setActiveTab={setActiveTab}> 
-          <Tab.Panel title="skz">StrayKids</Tab.Panel>
-          <Tab.Panel title="xh">XdinaryHeroes</Tab.Panel>
-          <Tab.Panel title="xg">XdinaryGirls</Tab.Panel>
-        </Tab>
-      </div>
-
-      <div style={{padding: '20px'}}>
-
-        {/* <Button text="XDINARYHEROES" /> */}
-        <Button> XDINARYHEROES&STRAYKIDS1 </Button> 
-        {/* when its like that the text called 'children' */}
-        <br />
-        <Button variant="success">XDINARYHEROES&STRAYKIDS2</Button>
-        <br />
-        <Button  variant="warning">XDINARYHEROES&STRAYKIDS3</Button>
-        <br />
-        <Button  variant="danger">XDINARYHEROES&STRAYKIDS4</Button>
-      
-      </div>
-
-      <h1 style={{color: 'white', background: 'red'}}>sasaStaaaLlain.com</h1>
-
-      <br />
-
-      <label htmlFor="search" onClick={searchFunction}>Search</label>
-      <input type="text" id="search" style={{background: 'grey'}} />
-
-      <ul>
-        {
-          todos.map(todo => 
-            <li>
-            {todo}
-          </li>
-        )}
-      </ul>
-
+      <button onClick={() => setShow(show => !show)}>{ show ? 'Hide' : 'Show' }</button>
+      { show && <Test /> }
     </>
   )
 }
 
-export default App;
+export default App
--- src/Test.tsx
+++ src/Test.tsx
@@ -1,7 +1,47 @@
-function Test() {
+import { useEffect, useState } from "react"
+
+export default function Test() {
+
+    const [postId, setPostId] = useState(1)
+    const [post, setPost] = useState(false)
+
+    useEffect(() => {
+        console.log('Components run when first loaded.')
+        fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+        .then(response => response.json())
+        .then(json => setPost(json))
+        // But when we add this here the log work only once
+        let interval = setInterval(() => console.log('Interval worked'), 1000)
+        return () => {
+            console.log('Components destroyed')
+            clearInterval(interval)
+            // Like that the interval will be destroyed
+        }
+    }, [])
+
+/*
+    useEffect(() => {
+        console.log('Components is been render')
+    })
+*/
+
+    useEffect(() => {
+        // console.log('PostId is been changed = ', postId)
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+        .then(res => res.json())
+        .then(data => setPost(data))
+    }, [postId])
+    // When we use like this only the log gonna work every time we push the button
+
+
     return (
-        <div className="App">Test.tsx file component</div>
+        <div>
+            <h3>{postId}</h3>
+            { post && JSON.stringify(post) }
+            <button onClick={() => setPostId(postId => postId + 1)}>Topics</button>
+            <hr />
+            Test.tsx file component
+        </div>
     )
 }
 
-export default Test;
